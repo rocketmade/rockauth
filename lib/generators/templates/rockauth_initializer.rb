3354b543cfba1bb780a2f3f4f2b97bffaba9cad4
Daniel Evans <evans.daniel.n@gmail.com>
2015-07-15 02:22:15 -0600

    Remove extra template

--- lib/generators/templates/rockauth_initializer.rb
+++
@@ -1,6 +0,0 @@
-Rockauth.configure do |config|
-  # config.allowed_password_length = 8..72
-  # config.email_regexp = /\A[^@\s]+@([^@\s]+\.)+[^@\W]+\z/
-  # config.token_time_to_live = 365 * 24 * 60 * 60
-  # config.clients = []
-end
