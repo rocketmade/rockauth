6bb4db1a0d3aaf8767a8e00990c4805825211f1a
Daniel Evans <evans.daniel.n@gmail.com>
2015-07-28 09:41:13 -0600

    Define ControllerError properly

--- lib/rockauth/errors.rb
+++ lib/rockauth/errors.rb
@@ -2,7 +2,7 @@ require 'active_model'
 
 module Rockauth
   module Errors
-    ControllerError < Struct.new(:status_code, :message, :validation_errors) do
+    ControllerError = Struct.new(:status_code, :message, :validation_errors) do
       include ActiveModel::Serialization
       def self.active_model_serializer
         Rockauth::Configuration.serializers.error.safe_constantize
