d187ea2a57ac86a4a7e56a0853d0c457cb00a01b
Daniel Evans <evans.daniel.n@gmail.com>
2015-10-22 11:31:41 -0600

    Validate email uniqueness

--- app/models/rockauth/user.rb
+++ app/models/rockauth/user.rb
@@ -7,8 +7,9 @@ module Rockauth
 
     resource_owner
 
-    validates_presence_of :email, if: :email_required?
-    validates_format_of   :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\W]+\z/, if: -> { (new_record? || email_changed?) }, allow_blank: true
+    validates_presence_of   :email, if: :email_required?
+    validates_uniqueness_of :email, case_sensitive: false, allow_nil: true
+    validates_format_of     :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\W]+\z/, if: -> { (new_record? || email_changed?) }, allow_blank: true
 
     has_secure_password   validations: false
     validates_presence_of :password, if: :password_required?
