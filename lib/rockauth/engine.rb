4d83fa55474b9fd7baa8c89647db5069e4afe231
Daniel Evans <evans.daniel.n@gmail.com>
2016-01-07 17:46:15 -0700

    Hide admin resources from Rails eager loading

--- lib/rockauth/engine.rb
+++ lib/rockauth/engine.rb
@@ -8,7 +8,7 @@ module Rockauth
 
     initializer "rockauth.load_active_admin_resources" do
       if Rockauth::Configuration.generate_active_admin_resources || Rockauth::Configuration.generate_active_admin_resources.nil? && defined?(ActiveAdmin)
-        ActiveAdmin.application.load_paths += [File.expand_path(File.dirname(__FILE__) + '../../../app/admin')]
+        ActiveAdmin.application.load_paths += [File.expand_path(File.join(File.dirname(__FILE__), '../rockauth/admin'))]
       end
     end
   end
