43f1a5507cbba0c9614557de5271e28de85f8fd3
Daniel Evans <evans.daniel.n@gmail.com>
2016-01-13 12:46:45 -0700

    Use a generator flow instead of magic reflection for active admin

---
+++ lib/generators/templates/provider_authentication_admin.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register ProviderAuthentications, as: "ProviderAuthentication" do
+  menu parent: Rockauth::Configuration.active_admin_menu_name
+
+  actions :all, except: %i(new create edit update)
+
+
+  index do
+    id_column
+    column :resource_owner
+    column :provider
+    column :provider_user_id
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      (resource.attribute_names.map(&:to_sym) - %i(provider_access_token provider_access_token_secret)).each do |key|
+        row key
+      end
+    end
+    active_admin_comments
+  end
+end
